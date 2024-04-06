5a065667887ea10441be5ca4547555552882b057
Régis N. <ndizihiweregis06@gmail.com>
2024-04-06 02:01:01 +0200

    feat: dynamic inputs

---
+++ src/lib/components/Input.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	export let placeholder: string;
+	export let label: string;
+	export let value: any;
+	export let type: 'text' | 'textarea' | 'select' | 'email' | 'password' | 'number';
+	export let options: any[] = [];
+</script>
+
+<div class="w-full flex flex-col gap-2">
+	{#if label.trim() !== ''}
+		<span class="text-label">{label}</span>
+	{/if}
+
+	{#if type !== 'textarea' && type !== 'select'}
+		<input
+			{type}
+			class="w-full border border-[#F0F4FD] placeholder:text-brand-lightblack/40 placeholder:text-sm h-[50px] rounded-md px-5 focus:ring-brand-darkblue/50 focus:ring-2 duration-100 outline-none"
+			{placeholder}
+			on:input={(e) => (value = e.currentTarget.value)}
+		/>
+	{:else if type === 'textarea'}
+		<textarea
+			class="w-full border border-[#F0F4FD] placeholder:text-brand-lightblack/40 placeholder:text-sm h-[50px] rounded-md px-5 min-h-[140px] outline-none py-3 resize-none focus:ring-brand-darkblue/50 focus:ring-2 duration-100"
+			{placeholder}
+			bind:value
+		/>
+	{:else}
+		<select
+			class="w-full border border-[#F0F4FD] placeholder:text-brand-lightblack/40 placeholder:text-sm h-[50px] rounded-md px-5 outline-none focus:ring-brand-darkblue/50 focus:ring-2 duration-100"
+			bind:value
+		>
+			{#each options as option}
+				<option value={option.value}>{option.label}</option>
+			{/each}
+		</select>
+	{/if}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
-<script>
-	import Hero from '$lib/sections/home/Hero.svelte';
+<script lang="ts">
+	import Input from '$lib/components/Input.svelte';
+	let name: string = '';
 </script>
 
 <svelte:head>
@@ -8,5 +9,9 @@
 </svelte:head>
 
 <section>
-	<Hero />
+	<!-- <Hero /> -->
+	<div class="p-32">
+		<h1 class="text-6xl">{name}</h1>
+		<Input label="name" placeholder="Enter name" type="textarea" bind:value={name} />
+	</div>
 </section>
